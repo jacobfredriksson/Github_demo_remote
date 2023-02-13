a5ea4e15c2cc368e16e5e9e7e4452cf2dcae521b
jacobfredriksson <jacobfredriksson@live.se>
2023-02-13 10:32:48 +0100

    Initial Commit

---
+++ Github_demo/Github_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_demoApp.swift
+//  Github_demo
+//
+//  Created by Jacob Fredriksson on 2023-02-13.
+//
+
+import SwiftUI
+
+@main
+struct Github_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
