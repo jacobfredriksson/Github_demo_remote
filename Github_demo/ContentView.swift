8af72782c86095d58f4b2e28a5d9f345350b21a8
jacobfredriksson <jacobfredriksson@live.se>
2023-02-13 10:36:12 +0100

    Change test content

--- Github_demo/ContentView.swift
+++ Github_demo/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("This is a test")
         }
         .padding()
     }
